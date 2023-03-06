4212702f067c4e95d31e90f74718c022563dda1b
Ravali <ravali@example.com>
2023-03-06 16:30:16 +0000

    todayDate

---
+++ src/main/java/com/example/date/MyController.java
@@ -0,0 +1,19 @@
+package com.example.date;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class MyController {
+    
+    @GetMapping("/")
+    public String Date(){
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = currentDate.format(formatter);
+        return formattedDate;
+    }
+
+    
+}
